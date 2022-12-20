3419f8658c4fa3d71e55a40b24db4062d3fc729e
Spike^ekipS <spikeekips@gmail.com>
2022-12-20 12:23:13 +0900

    BuildInfo applied

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/alecthomas/kong"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/spikeekips/mitum/launch"
+	"github.com/spikeekips/mitum/util"
 	mitumlogging "github.com/spikeekips/mitum/util/logging"
+)
+
+var (
+	Version   = "v0.0.1"
+	BuildTime = "-"
+	GitBranch = "-"
+	GitCommit = "-"
 )
 
 var (
@@ -28,9 +39,21 @@ func main() {
 	var cli struct {
 		launch.LoggingFlags `embed:"" prefix:"log."`
 		Run                 runCommand `cmd:"" help:"run contest"`
+		Version             struct{}   `cmd:"" help:"version"`
 	}
 
 	kctx := kong.Parse(&cli, kongOptions...)
+
+	bi, err := util.ParseBuildInfo(Version, GitBranch, GitCommit, BuildTime)
+	if err != nil {
+		kctx.FatalIfErrorf(err)
+	}
+
+	if kctx.Command() == "version" {
+		_, _ = fmt.Fprintln(os.Stdout, bi.String())
+
+		return
+	}
 
 	l, err := launch.SetupLoggingFromFlags(cli.LoggingFlags)
 	if err != nil {
